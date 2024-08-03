9bed7b078e7d84a133c37e696e5424e4bde8510e
onlyhouse1980 <56930138+onlyhouse1980@users.noreply.github.com>
2024-08-03 06:35:53 +0200

    Add files via upload

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <div className='grainy-light'>
-      <section className='bg-gray-100'>
+      <section className='bg-white'>
         <MaxWidthWrapper className='pb-24 pt-10 lg:grid lg:grid-cols-3 sm:pb-32 lg:gap-x-0 xl:gap-x-8 lg:pt-24 xl:pt-32 lg:pb-52'>
           <div className='col-span-2 px-6 lg:px-0 lg:pt-4'>
             <div className='relative mx-auto text-center lg:text-left flex flex-col items-center lg:items-start'>
